c1210eefad167bb24e04eb4fda3163d65d14de7a
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-09-26 22:48:35 +0900

    fix: App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,28 @@ function App() {
   useEffect(() => {
     // Kakao 지도 API 스크립트를 비동기로 로드
     const loadKakaoMapScript = () => {
-        return new Promise((resolve, reject) => {
-            const kakaoAppKey = import.meta.env.VITE_KAKAO_APP_KEY;
-            const script = document.createElement('script');
-            script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoAppKey}&autoload=false`;  // autoload=false 옵션 사용
-            script.onload = () => {
-                window.kakao.maps.load(() => { // Kakao API가 완전히 로드된 후에 실행
-                    setIsKakaoLoaded(true);
-                    resolve();
-                });
-            };
-            script.onerror = () => reject(new Error('Kakao Map API script failed to load'));
-            document.head.appendChild(script);
-        });
+      return new Promise((resolve, reject) => {
+        const kakaoAppKey = import.meta.env.VITE_KAKAO_APP_KEY;
+        const script = document.createElement('script');
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoAppKey}&autoload=false`;  // autoload=false 옵션 사용
+        script.onload = () => {
+          window.kakao.maps.load(() => { // Kakao API가 완전히 로드된 후에 실행
+            setIsKakaoLoaded(true);
+            resolve();
+          });
+        };
+        script.onerror = () => reject(new Error('Kakao Map API script failed to load'));
+        document.head.appendChild(script);
+      });
     };
 
     loadKakaoMapScript().catch(err => {
-        console.error('Kakao Maps API 로드 실패:', err);
+      console.error('Kakao Maps API 로드 실패:', err);
     });
   }, []);
 
   if (!isKakaoLoaded) {
-      return <div>Loading Kakao Maps...</div>;
+    return <div>Loading Kakao Maps...</div>;
   }
   
   return (
